770a26b611746e502996019b25d75d35fa544b79
unknown <richard_allcca_llano@hotmail.com>
2024-07-28 10:59:55 -0500

    added extract element of page target

--- src/with-proxies/index.js
+++ src/with-proxies/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import puppeteer from "puppeteer";
 
 const main = async () => {
@@ -13,8 +14,41 @@ const main = async () => {
   // El obj de configuración le dice que espere mientras la pagina carga
   await page.goTo(url, { waitUntil: 'networkidle2' });
 
-  const tittle = await page.title();
-  console.log(`Titulo de la pagina: ${tittle}`);
+  // const tittle = await page.title();
+  // console.log(`Titulo de la pagina: ${tittle}`);
+
+  let product = [];
+  let nexPage = true;
+
+  while (nexPage) {
+    // Evaluate usa el inspector de elementos
+    const newProducts = await page.evaluate(() => {
+      const $products = document.querySelectorAll('.product-card');
+      const mappedProducts = [];
+
+      $products.forEach(($product) => {
+        mappedProducts.push({
+          title: $product.querySelector('.product-title').textContent,
+          price: $product.querySelector('.product-price').textContent,
+          priceWhole: $product.querySelector('.product-price-whole').textContent,
+          image: $product.querySelector('.product-image').src,
+        });
+      });
+
+      return mappedProducts;
+    });
+
+    product = product.concat(newProducts);
+
+    const nextPageButton = await page.$('.next-page-button');
+
+    if (nextPageButton) {
+      await nextPageButton.click();
+      await page.waitForNetworkIdle();
+    } else {
+      nexPage = false;
+    }
+  }
 
 };
 
